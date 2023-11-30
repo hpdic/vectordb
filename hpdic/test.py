5faab7d638e2f22fa075ed70d279ee8c918b01a2
Dongfang Zhao <dzhao@uw.edu>
2023-11-30 07:53:48 +0000

    hnsw works

--- hpdic/test.py
+++ hpdic/test.py
@@ -10,7 +10,8 @@ import numpy as np
 from vectordb import InMemoryExactNNVectorDB, HNSWVectorDB
 
 # Specify your workspace path
-db = InMemoryExactNNVectorDB[ToyDoc](workspace='./workspace_path')
+# db = InMemoryExactNNVectorDB[ToyDoc](workspace='./workspace_path')
+db = HNSWVectorDB[ToyDoc](workspace='./workspace_path')
 
 # Index a list of documents with random embeddings
 doc_list = [ToyDoc(text=f'toy doc {i}', embedding=np.random.rand(128)) for i in range(1000)]
@@ -22,8 +23,10 @@ results = db.search(inputs=DocList[ToyDoc]([query]), limit=10)
 
 # Print out the matches
 i = 0
+len_res = len(results[0].matches)
 for m in results[0].matches:
-  print(m)
+  if i == len_res - 1:
+    print(m)
   print(f'{i+1} matching\n')
   i += 1
   
